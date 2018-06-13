49de99e3f07284ea04fe8f3f9e8c2d8ad3d77788
Juan Camilo Pérez <jc.perez13@uniandes.edu.co>
2018-06-13 08:57:11 -0500

    added counting ability on chadsvasc score

--- js/script.js
+++ js/script.js
@@ -26,11 +26,11 @@ function validate_number(evt) {
 // Taken from
 // https://stackoverflow.com/questions/8838648/onchange-event-handler-for-radio-button-input-type-radio-doesnt-work-as-one
 var rad = document.yes_no_radios.yes_no;
-var prev = null;
-for(var i = 0; i < rad.length; i++) {
+var prev_yes_no = null;
+for (var i = 0; i < rad.length; i++) {
     rad[i].onclick = function() {
-        if(this !== prev) {
-            prev = this;
+        if(this !== prev_yes_no) {
+            prev_yes_no = this;
             // Do stuff here
             if(this.value === 'yes'){
             	$('#CHA2DS2-VAS2C').show();
@@ -41,3 +41,19 @@ for(var i = 0; i < rad.length; i++) {
         }
     };
 }
+
+// Manage changes in CHADSVASC
+function chadsvasc(radio_names) {
+	var radios = document.getElementById('CHA2DS2-VAS2C').getElementsByTagName('input');
+	var score = 0;
+	for (var i = 0; i < radios.length; i++) {
+		if(radios[i].checked) {
+			score += radios[i].value;
+		}
+	}
+	// $('#label-puntaje-chadsvasc').empty();
+	$('#label-puntaje-chadsvasc').text(score.toString());
+}
+
+
+
