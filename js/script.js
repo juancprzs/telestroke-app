69bab179433655c21d3f2e48a2eda361e03bbbe1
Juan Camilo Pérez <jc.perez13@uniandes.edu.co>
2018-06-13 20:52:53 -0500

    changed management of tables and added up to examen fisico

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
+var used_chadsvasc = false;
+var filled_chadsvasc = false;
+var filled_glasgow = false;
+
 $(document).ready(function() {
-	$('#descripcion-sintomas').hide();
-	// Validación de información
-	$('#validar-informacion').on('click', function() {
-		alert("Datos validados.");
-		$('#descripcion-sintomas').show();
-		$('#CHA2DS2-VAS2C').hide();
-	});
+	$('#modulo-historia-clinica').hide();
+	$('#modulo-examen-fisico').hide();
 });
+
+function validate_info() {
+	alert("Datos validados.");
+	$('#modulo-historia-clinica').show();
+	$('#CHA2DS2-VAS2C').hide();
+	$('#anticoagulantes-orales').hide();
+	$('#modulo-examen-fisico').hide();
+}
+
 
 // validate if input is number (prevent writing)
 // Taken from 
@@ -29,31 +37,124 @@ var rad = document.yes_no_radios.yes_no;
 var prev_yes_no = null;
 for (var i = 0; i < rad.length; i++) {
     rad[i].onclick = function() {
-        if(this !== prev_yes_no) {
+        if (this !== prev_yes_no) {
             prev_yes_no = this;
             // Do stuff here
-            if(this.value === 'yes'){
+            if (this.value === 'yes'){
             	$('#CHA2DS2-VAS2C').show();
+            	used_chadsvasc = true;
             }
             else {
             	$('#CHA2DS2-VAS2C').hide();
+            	used_chadsvasc = false;
             }
+        }
+        $('#anticoagulantes-orales').show();
+
+        // Taken from
+		// https://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
+		if (used_chadsvasc) {
+	    	if (!filled_chadsvasc) {
+	        	document.getElementById("continuar-examen-fisico").className =
+						   document.getElementById("continuar-examen-fisico").className.replace
+						      ( /(?:^|\s)active(?!\S)/g , ' disabled' );
+	     	}
+	     	else {
+	        		document.getElementById("continuar-examen-fisico").className =
+						   document.getElementById("continuar-examen-fisico").className.replace
+						      ( /(?:^|\s)disabled(?!\S)/g , ' active' );
+	    	}
+	    }
+        else {
+        	document.getElementById("continuar-examen-fisico").className =
+					   document.getElementById("continuar-examen-fisico").className.replace
+					      ( /(?:^|\s)disabled(?!\S)/g , ' active' );
         }
     };
 }
 
 // Manage changes in CHADSVASC
-function chadsvasc(radio_names) {
+function chadsvasc() {
 	var radios = document.getElementById('CHA2DS2-VAS2C').getElementsByTagName('input');
+	var questions = 7;
+	var checked = 0;
 	var score = 0;
 	for (var i = 0; i < radios.length; i++) {
-		if(radios[i].checked) {
+		if (radios[i].checked) {
+			checked += 1;
 			score += parseInt(radios[i].value);
 		}
 	}
-	// $('#label-puntaje-chadsvasc').empty();
+	if (checked === questions) {
+		if ($('#chadsvasc-completado').text() !== 'Completa') {
+			alert('Escala CHADSVASC fue completada');
+			filled_chadsvasc = true;
+		}
+		$('#chadsvasc-completado').text('Completa');
+	}
 	$('#label-puntaje-chadsvasc').text(score.toString());
+
+	// Change button from disabled to active or viceversa
+	if (used_chadsvasc) {
+    	if (!filled_chadsvasc) {
+        	document.getElementById("continuar-examen-fisico").className =
+					   document.getElementById("continuar-examen-fisico").className.replace
+					      ( /(?:^|\s)active(?!\S)/g , ' disabled' );
+     	}
+     	else {
+        		document.getElementById("continuar-examen-fisico").className =
+					   document.getElementById("continuar-examen-fisico").className.replace
+					      ( /(?:^|\s)disabled(?!\S)/g , ' active' );
+    	}
+    }
 }
+
+function list_anticoagulantes() {
+	var cbs = document.getElementById('anticoagulantes-orales').getElementsByTagName('input');
+	var elements = [];
+	for (var i = 0; i < cbs.length; i++) {
+		if (cbs[i].checked) {
+			elements.push(cbs[i].value);
+		}
+	}
+	if (elements.length === 0){
+		// Empty array
+		lista = 'Ninguno';
+	}
+	else {
+		lista = elements.join(', ');
+	}
+	$('#lista_de_anticoagulantes').text(lista);
+}
+
+function continue_phys_exam() {
+	console.log('phys exam')
+	$('#modulo-examen-fisico').show();
+}
+
+function glasgow() {
+	var radios = document.getElementById('glasgow').getElementsByTagName('input');
+	var questions = 3;
+	var checked = 0;
+	var score = 0;
+	for (var i = 0; i < radios.length; i++) {
+		if (radios[i].checked) {
+			checked += 1;
+			score += parseInt(radios[i].value);
+		}
+	}
+	if (checked === questions) {
+		if ($('#glasgow-completado').text() !== 'Completa') {
+			alert('Escala Glasgow fue completada');
+			filled_glasgow = true;
+		}
+		$('#glasgow-completado').text('Completa');
+	}
+	$('#label-puntaje-glasgow').text(score.toString());
+
+}
+
+
 
 
 
